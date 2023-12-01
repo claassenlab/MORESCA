5ec7d82d524bae1588b638d6208e40e02a780f32
Matthias Bruhns <45033731+mbruhns@users.noreply.github.com>
2023-12-01 15:51:06 +0100

    14 auto mode for qc (#42)
    * Simplify ddqc.
    
    * Add ddqc to auto-mode for MT.
    
    * ddqc
    
    * Add ddqc changes.

--- MORESCA/utils.py
+++ MORESCA/utils.py
@@ -88,7 +88,10 @@ def remove_cells_by_pct_counts(
             else:
                 adata = adata[adata.obs[f"pct_counts_{genes}"] < threshold, :]
         case "auto":
-            raise ValueError(f"Auto selection for {genes}_threshold not implemented.")
+            if genes == "mt":
+                adata = ddqc(adata, inplace=False)
+            else:
+                raise ValueError(f"Auto selection for {genes}_threshold not implemented.")
         case False | None:
             print(f"No {genes} filter applied.")
         case _:
@@ -126,7 +129,7 @@ def remove_genes(gene_lst: list, rmv_lst: list, gene_key) -> None:
             raise ValueError("Invalid choice for gene_key.")
 
 
-def ddqc(adata: AnnData, inplace: bool = True, save: bool = False) -> Optional[AnnData]:
+def ddqc(adata: AnnData, inplace: bool = True) -> Optional[AnnData]:
     """
     Perform Data-Driven Quality Control (DDQC) on an AnnData object.
 
@@ -140,6 +143,9 @@ def ddqc(adata: AnnData, inplace: bool = True, save: bool = False) -> Optional[A
 
     Note:
         - The resulting AnnData object will only contain cells that pass the quality control checks.
+
+    Todo:
+        - The clustering here is possibly not equivalent to our later clustering routine. Should this match?
     """
 
     if not inplace:
@@ -147,11 +153,12 @@ def ddqc(adata: AnnData, inplace: bool = True, save: bool = False) -> Optional[A
 
     adata_raw = adata.copy()
 
-    adata_raw = adata.copy()
     sc.pp.calculate_qc_metrics(
         adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True
     )
     adata = adata[adata.obs.pct_counts_mt <= 80, :]
+
+    # Todo: can this be removed?
     adata.layers["counts"] = adata.X.copy()
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
@@ -161,41 +168,24 @@ def ddqc(adata: AnnData, inplace: bool = True, save: bool = False) -> Optional[A
     sc.pp.scale(adata)
     sc.tl.pca(adata)
     sc.pp.neighbors(adata, n_neighbors=20, n_pcs=50, metric="euclidean")
-    sc.tl.leiden(adata, resolution=1.0)
+    sc.tl.leiden(adata, resolution=1.4)
 
-    # Create a numpy array to store the passing information
-    n_obs = adata.n_obs
-    passed = np.ones(n_obs, dtype=bool)
+    # Directly apply the quality control checks and create the 'passed' mask
+    passed = np.ones(adata.n_obs, dtype=bool)
+    for cluster in adata.obs["leiden"].unique():
+        indices = adata.obs["leiden"] == cluster
+        pct_counts_mt_cluster = adata.obs.loc[indices, "pct_counts_mt"].values
+        total_counts_cluster = adata.obs.loc[indices, "total_counts"].values
+        n_genes_cluster = adata.obs.loc[indices, "n_genes"].values
 
-    # Map clusters to contiguous integer labels
-    cluster_idx_map = {
-        cluster: np.where(adata.obs["leiden"].values == cluster)[0]
-        for cluster in adata.obs["leiden"].unique()
-    }
-
-    # Iterate through each cluster
-    for indices in cluster_idx_map.values():
-        # Subset relevant cluster data
-        pct_counts_mt_cluster = adata.obs["pct_counts_mt"].values[indices]
-        total_counts_cluster = adata.obs["total_counts"].values[indices]
-        n_genes_cluster = adata.obs["n_genes"].values[indices]
-
-        # Apply the upper bound check for pct_counts_mt
         passing_mask_mt = is_passing_upper(pct_counts_mt_cluster, nmads=3)
-
-        # Apply the lower bound check for n_counts and n_genes
         passing_mask_counts = is_passing_lower(
             total_counts_cluster, nmads=3, lower_limit=0
         )
         passing_mask_genes = is_passing_lower(n_genes_cluster, nmads=3, lower_limit=200)
 
-        # Combine the masks
-        full_passing_mask = passing_mask_mt & passing_mask_counts & passing_mask_genes
+        passed[indices] = passing_mask_mt & passing_mask_counts & passing_mask_genes
 
-        # Update the 'passed' numpy array using the cluster indices
-        passed[indices] = full_passing_mask
-
-    # Subselect 'adata' based on the 'passed' mask
     adata = adata_raw[passed].copy()
 
     if not inplace:
