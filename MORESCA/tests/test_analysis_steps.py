138fc04526d8a66c299b7430c5bc07c928b43a11
Sebastian Bischoff <sebastian@salzreute.de>
2023-12-07 15:28:45 +0100

    Isolate tests by using a fresh copy of the data before each test

--- MORESCA/tests/test_analysis_steps.py
+++ MORESCA/tests/test_analysis_steps.py
@@ -2,6 +2,7 @@ import os.path
 
 import gin
 import scanpy as sc
+import pytest
 
 from MORESCA.analysis_steps import (
     batch_effect_correction,
@@ -16,11 +17,11 @@ from MORESCA.analysis_steps import (
 
 # PATH_H5AD = Path("../data/data_raw.h5ad")
 # STR_H5AD = "../data/data_raw.h5ad"
-ADATA = sc.datasets.pbmc3k()
+ADATA_original = sc.datasets.pbmc3k()
 # sc.read(PATH_H5AD)
-ADATA.layers["counts"] = ADATA.X.copy()
-ADATA_BATCH = ADATA.copy()
-ADATA_BATCH.obs["batch"] = 1350 * ["a"] + 1350 * ["b"]
+ADATA_original.layers["counts"] = ADATA_original.X.copy()
+
+ADATA = None
 
 gin.parse_config_file("test-config.gin")
 
@@ -29,6 +30,13 @@ gin.parse_config_file("test-config.gin")
 # @pytest.mark.parametrize("test_data_path", [(PATH_H5AD), (STR_H5AD)])
 # def test_load_data(test_data_path):
 #    assert load_data(test_data_path)
+
+
+# SOURCE: https://stackoverflow.com/a/22638709
+@pytest.fixture(autouse=True)
+def run_before_tests():
+    global ADATA
+    ADATA = ADATA_original.copy()
 
 
 def test_quality_control():
@@ -85,5 +93,8 @@ def test_plotting_umap_with_pca():
 
 
 def test_batch_effect_correction():
+    ADATA_BATCH = ADATA
+    ADATA_BATCH.obs["batch"] = 1350 * ["a"] + 1350 * ["b"]
+
     pca(ADATA_BATCH, use_highly_variable=False)
     batch_effect_correction(adata=ADATA_BATCH)
