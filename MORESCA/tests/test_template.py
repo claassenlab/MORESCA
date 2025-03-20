9f9bd444539ae5d9fc76faf87a2ac20cbc9a3171
Jan Schleicher <jan.schleicher@uni-tuebingen.de>
2025-03-20 13:24:34 +0100

    Run processing per data set if multiple paths are passed from the command line.

--- MORESCA/tests/test_template.py
+++ MORESCA/tests/test_template.py
@@ -1,5 +1,8 @@
+import shutil
+import subprocess
 from pathlib import Path
 
+import pytest
 import scanpy as sc
 
 from MORESCA.template import run_analysis
@@ -8,9 +11,44 @@ ADATA = sc.datasets.pbmc3k()
 ADATA.obs["batch"] = 1350 * ["a"] + 1350 * ["b"]
 DATA_PATH = Path("data/data_raw.h5ad")
 ADATA.write_h5ad(DATA_PATH)
+CONFIG_PATH = Path("test-config.gin")
 
 
 def test_run_analysis():
-    run_analysis(
-        DATA_PATH, config_path=Path("test-config.gin"), figures=False, verbose=False
+    run_analysis(DATA_PATH, config_path=CONFIG_PATH, verbose=False)
+
+
+@pytest.mark.parametrize(
+    "data_path, output_path",
+    [
+        ([DATA_PATH], ["res/1", "res/2"]),
+        ([DATA_PATH, DATA_PATH, DATA_PATH], ["res/1", "res/2"]),
+    ],
+)
+def test_run_analysis_exception(data_path: list, output_path: list):
+    with pytest.raises(
+        ValueError, match="Incompatible values for `result_path` and `data_path`"
+    ):
+        run_analysis(
+            data_path=data_path, config_path=CONFIG_PATH, result_path=output_path
+        )
+
+
+@pytest.mark.parametrize(
+    "data_path, output_path",
+    [
+        ([DATA_PATH], ["res"]),
+        ([DATA_PATH, DATA_PATH], ["res"]),
+        ([DATA_PATH, DATA_PATH], ["res/1", "res/2"]),
+    ],
+)
+def test_template(data_path: list, output_path: list):
+    cmd = (
+        ["python3", "../template.py", "-d"]
+        + data_path
+        + ["-o"]
+        + output_path
+        + ["-p", CONFIG_PATH]
     )
+    subprocess.run(cmd, check=True)
+    shutil.rmtree("res")
